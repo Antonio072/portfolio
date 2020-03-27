31220b499a05458aca0e73587726ec91347d6914
Jose Antonio <16030681@itcelaya.edu.mx>
2020-03-26 21:16:48 -0500

    Add initial configuration

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from "react"
+import illustration from '../imgs/undraw_code_thinking_1jeh.svg'
+import Form from "./contact.form"
+
+export default () => (
+    <header className="bg-gray-300 ">
+        <div class="container mx-auto p-12 max-w-4xl ">
+            <div className="flex justify-center items-center">
+                <div class="flex-2">
+                    <h1 className="text-6xl font-bold text-purple-700">Hola mi nombre es Antonio</h1>
+                    <p className="text-xl font-light ">Me gusta crear cosas y enseñar lo que me gusta ❤</p>
+                </div>
+                <img class="flex" src={illustration} alt="Profile"></img>
+            </div>
+        <Form/>
+        </div>
+    </header>
+)
