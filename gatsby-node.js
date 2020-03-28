d32b439a76dd42719291af1714c7c9d065ab901d
Jose Antonio <16030681@itcelaya.edu.mx>
2020-03-27 21:34:30 -0500

    Add generic Graphql pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,37 @@
 
 const path = require('path')
 
-exports.createPages = async ({graphql, actions}) => {
+exports.createPages = async ({ graphql, actions }) => {
+    
+    const result = await graphql(`
+    {
+        allEducationJson {
+          edges {
+            node {
+              slug
+            }
+          }
+        }
+      }
+      
+    `)
+    console.log(result);
+    
+    result.data.allEducationJson.edges.forEach(element => {
+        
+        const { node } = element
+        console.log(node);
+        
+        actions.createPage({
+             path: node.slug,
+             component: path.resolve('./src/components/template.js'),
+             contest:{
+                 slug: node.slug
+             }
+         })
+    });
+
+
     actions.createPage({
         path: 'dummy',
         component: path.resolve('./src/components/template.js')
